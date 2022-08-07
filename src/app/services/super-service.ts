d972486344b89ab1ee5c0d381c1d452714253f02
Udara Jayawardena <udara@earltech.biz>
2022-08-07 09:48:43 +0530

    dev_config_update

--- src/app/services/super-service.ts
+++ src/app/services/super-service.ts
@@ -2,8 +2,7 @@ export class SuperService {
 
   public getBaseUrl(): string {
     // return 'http://localhost:3000/api';
-    return 'http://ec2-3-111-113-150.ap-south-1.compute.amazonaws.com:3000/api';
-    
+    return 'http://ec2-3-111-113-150.ap-south-1.compute.amazonaws.com:3000/api';  
   }
   public getEmailUrl(): string {
     // return 'http://localhost:3000/';
