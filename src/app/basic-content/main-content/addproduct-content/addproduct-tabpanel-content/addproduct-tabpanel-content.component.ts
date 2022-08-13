4862b8fa06b42661a4645e5b169a6bb60c0b3d7c
Nuwan Nadeera <47295198+nuwannadeera@users.noreply.github.com>
2022-08-13 21:05:47 +0530

    admin changes

--- src/app/basic-content/main-content/addproduct-content/addproduct-tabpanel-content/addproduct-tabpanel-content.component.ts
+++ src/app/basic-content/main-content/addproduct-content/addproduct-tabpanel-content/addproduct-tabpanel-content.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {AddproductServiceService} from '../../../../services/addproduct-service.service';
 import {Categories} from '../../../../dto/Categories';
 import {Product} from '../../../../dto/Product';
@@ -12,6 +12,8 @@ import * as spect from '../spect.json';
 import {ProductServiceService} from '../../../../services/product-service.service';
 import {RentalItem} from '../../../../dto/RentalItem';
 import {Gallery} from '../../../../dto/Gallery';
+import {forEach} from '@angular/router/src/utils/collection';
+import {truncate} from 'fs';
 
 @Component({
   selector: 'app-addproduct-tabpanel-content',
@@ -28,9 +30,9 @@ export class AddproductTabpanelContentComponent implements OnInit {
   };
 
   // Form model content changed.
-  writeValue(content: any): void {
-    this.model = content;
-  }
+  // writeValue(content: any): void {
+  //   this.model = content;
+  // }
 
   registerOnChange(fn: (_: any) => void): void {
     this.onChange = fn;
@@ -41,9 +43,6 @@ export class AddproductTabpanelContentComponent implements OnInit {
   }
 
   // End ControlValueAccesor methods.
-
-  highlightModel: any;
-  model: any;
 
   config: Object = {
     charCounterCount: false,
@@ -59,9 +58,9 @@ export class AddproductTabpanelContentComponent implements OnInit {
   }
 
   // Form modelSpecs content changed.
-  writeValues(content: any): void {
-    this.modelSpecs = content;
-  }
+  // writeValues(content: any): void {
+  //   this.modelSpecs = content;
+  // }
 
   registerOnChanges(fn: (_: any) => void): void {
     this.onChanges = fn;
@@ -73,12 +72,14 @@ export class AddproductTabpanelContentComponent implements OnInit {
 
   // End ControlValueAccesor methods.
 
-  modelSpecs: any;
+  // modelSpecs: any;
 
   public editorContentforSpecs: string;
+  public highlightModel: string;
+  public overViewModel: string;
 
   public optionsSpecs: Object = {
-    placeholderText: 'Edit Your Content Here!',
+    placeholderText: 'Add Content Here!',
     charCounterCount: false
   };
 
@@ -106,6 +107,36 @@ export class AddproductTabpanelContentComponent implements OnInit {
   selectedProId: number;
   rentalimg: string;
 
+  product_name: string = "";
+  item_product: string = "";
+  item_category: string = "";
+  item_name: string = "";
+  item_brand:string = "";
+  item_qty: string = "";
+  item_price: string = "";
+  item_rate: string = "";
+  item_discount: string = "";
+  item_highlights: string = "";
+  item_spects: string = "";
+  item_overview: string = "";
+  category_name: string = "";
+
+
+  @ViewChild('productnameinput') productElement: ElementRef;
+  @ViewChild('itemprtype') item_productElement: ElementRef;
+  @ViewChild('itemcattype') item_categoryElement: ElementRef;
+  @ViewChild('itemnameinput') item_nameElement: ElementRef;
+  @ViewChild('itembrandinput') item_brandElement: ElementRef;
+  @ViewChild('itemqtyinput') item_qtyElement: ElementRef;
+  @ViewChild('itempriceinput') item_priceElement: ElementRef;
+  @ViewChild('itemrateinput') item_rateElement: ElementRef;
+  @ViewChild('itemdisinput') item_discountElement: ElementRef;
+  @ViewChild('highlights') item_highlightsElement: ElementRef;
+  @ViewChild('itemspects') item_spectsElement: ElementRef;
+  @ViewChild('itmoverview') item_overviewElement: ElementRef;
+  @ViewChild('catnameinput') category_nameElement: ElementRef;
+
+
   constructor(private upldfile: ImageserviceService, private addproductservice: AddproductServiceService,
               private productservice: ProductServiceService) {
   }
@@ -115,10 +146,116 @@ export class AddproductTabpanelContentComponent implements OnInit {
     this.getProductData(); //get all product data
     this.getAllItems(); //get all item data
     this.madeCheckbox(); //set all check box
-    this.getALlGalleryData(); //get all gallery data
+    // this.getALlGalleryData(); //get all gallery data
   }
 
-  //funtion of gell all gallery data
+  //---------------------------------saving validations
+  savingValidation(product, category) {
+    var check = true;
+    var msg1 = '';
+    var msg2 = '';
+    var msg3 = '';
+    if (product == "") {
+      console.log(product);
+      check = false;
+      msg1 = 'Add Product !';
+    }
+    if (category == "") {
+      console.log(category);
+      check = false;
+      msg2 = ' Add Category !';
+    }
+    if (this.mainspeclist.length == 0) {
+      console.log(this.mainspeclist.length);
+      check = false;
+      msg3 = ' Add Specification !';
+    }
+    if (!check) {
+    Swal.fire(msg1 + '\n' + msg2 + '\n' + msg3);
+    }
+    return check;
+  }
+
+  savingItemValidation(itemprtype,itemcattype,itemnameinput, itembrandinput, itemqtyinput, itempriceinput,
+                       imcount,highlightModel,overViewModel) {
+    var check = true;
+    var msg1 = '';
+    var msg2 = '';
+    var msg3 = '';
+    var msg4 = '';
+    var msg5 = '';
+    var msg6 = '';
+    var msg7 = '';
+    var msg8 = '';
+    var msg9 = '';
+    if (itemprtype == "") {
+      check = false;
+      msg1 = 'Add Product!';
+    }
+    if (itemcattype == "") {
+      check = false;
+      msg2 = ' Add Category!';
+    }
+    if (itemnameinput == "") {
+      check = false;
+      msg3 = ' Add Item Name!';
+    }
+    if (itembrandinput == "") {
+      check = false;
+      msg4 = ' Add Brand!';
+    }
+    if (itemqtyinput == "") {
+      check = false;
+      msg5 = ' Add Qty!';
+    }
+    if (itempriceinput == "") {
+      check = false;
+      msg6 = ' Add Price!';
+    }
+    if (imcount == "") {
+      check = false;
+      msg7 = ' Add Colour!';
+    }
+    if (highlightModel == undefined) {
+      check = false;
+      msg8 = ' Add Highlight!';
+    }
+    if (overViewModel == undefined) {
+      check = false;
+      msg9 = ' Add Over View!';
+    }
+    if (!check) {
+      Swal.fire(msg1 + msg2 + msg3 + msg4 + msg5 + msg6 + msg7 + msg8 + msg9);
+    }
+    return check;
+  }
+
+  //----------------------------------------clear function
+  clear() {
+    this.productElement.nativeElement.value = "";
+  }
+
+  clearItem() {
+    this.item_productElement.nativeElement.value = "";
+    this.item_categoryElement.nativeElement.value = "";
+    this.item_nameElement.nativeElement.value = "";
+    this.item_brandElement.nativeElement.value = "";
+    this.item_qtyElement.nativeElement.value = "";
+    this.item_priceElement.nativeElement.value = "";
+    this.item_rateElement.nativeElement.value = "";
+    this.item_discountElement.nativeElement.value = "";
+    this.item_highlightsElement.nativeElement.value = "";
+    this.item_spectsElement.nativeElement.value = "";
+    this.item_overviewElement.nativeElement.value = "";
+    this.imcount = "";
+  }
+
+  clearCategory() {
+    this.category_nameElement.nativeElement.value = "";
+  }
+
+
+  //---------------------------funtion of gell all gallery data
   getALlGalleryData() {
     this.productservice.getGalleryData()
       .subscribe((result: Array<Gallery>) => {
@@ -186,9 +323,26 @@ export class AddproductTabpanelContentComponent implements OnInit {
 
   //====================================================================================================================
 
-  //function of set textediter data
+  //functions of set textediter data
+
   setToTextEditer(val) {
     this.editorContentforSpecs = val;
+  }
+
+  getHighlightDetail(highlight) {
+    let lspec = highlight.replace(/,/g, '<br>');
+    this.setToHighlightsEditer(lspec);
+  }
+  setToHighlightsEditer(val) {
+    this.highlightModel = val;
+  }
+
+  getOverViewDetail(over_view) {
+    let ospec = over_view.replace(/,/g, '<br>');
+    this.setToOverViewEditer(ospec);
+  }
+  setToOverViewEditer(val) {
+    this.overViewModel = val;
   }
 
   //====================================================================================================================
@@ -199,14 +353,17 @@ export class AddproductTabpanelContentComponent implements OnInit {
       'power', 'physical', 'packaging_info'];
   }
 
+
   //====================================================================================================================
 
   //function of get color codes
   getCheckBoxValue(checkval: any, lbval) {
     if (this.mainspeclist.length == 0) {
+      console.log("mainspeclist.length ==0");
       this.mainspeclist.push(lbval);
-      console.log(this.mainspeclist + '==0');
+      console.log(this.mainspeclist);
     } else {
+      console.log("mainspeclist.length > 0");
       if (checkval.currentTarget.checked == true) {
         let count = 0;
         for (let i = 0; i < this.mainspeclist.length; i++) {
@@ -216,15 +373,15 @@ export class AddproductTabpanelContentComponent implements OnInit {
         }
         if (count == 0) {
           this.mainspeclist.push(lbval);
-
         }
-
+        console.log(this.mainspeclist);
       } else {
         for (let i = 0; i < this.mainspeclist.length; i++) {
           if (this.mainspeclist[i] == lbval) {
             this.mainspeclist.splice(i, 1);
           }
         }
+        console.log(this.mainspeclist);
       }
     }
   }
@@ -355,24 +512,31 @@ export class AddproductTabpanelContentComponent implements OnInit {
 
   // save category
   saveCategory(catname: string) {
-    let namee = this.validatee.validCategory(catname);
-    let catgry = new Categories(namee);
-    this.addproductservice.saveCategory(catgry)
-      .subscribe((result) => {
-          Swal.fire(
-            'Success',
-            'Category Added Successfully!',
-            'success'
-          );
-          this.getAddCategoryData();
-        }, (error1 => {
-        Swal.fire(
-          'Error',
-          error1.error.error.message + '',
-          'error'
+    console.log('catname');
+    console.log(catname);
+    if (catname == "") {
+      Swal.fire('Add Category !');
+    } else {
+      let namee = this.validatee.validCategory(catname);
+      let catgry = new Categories(namee);
+      this.addproductservice.saveCategory(catgry)
+        .subscribe((result) => {
+            Swal.fire(
+              'Success',
+              'Category Added Successfully!',
+              'success'
+            );
+            this.getAddCategoryData();
+            this.clearCategory();
+          }, (error1 => {
+            Swal.fire(
+              'Error',
+              error1.error.error.message + '',
+              'error'
+            );
+          })
         );
-        })
-      );
+    }
   }
 
   //===================================================================================================================
@@ -421,107 +585,62 @@ export class AddproductTabpanelContentComponent implements OnInit {
   // check item detail and save
   saveItem(itemnameinput, itembrandinput, itemqtyinput, itempriceinput,
            itemrateinput, itemdisinput, itemprtype, itemcattype) {
+    if (this.savingItemValidation(itemnameinput, itembrandinput, itemqtyinput, itempriceinput,
+      itemprtype, itemcattype,this.imcount,this.highlightModel,this.overViewModel)) {
+      for (var i = 0; i < this.imgarray.length; i++) {
+        this.emails += this.imgarray[i] + ',';
+      }
 
-    for (var i = 0; i < this.imgarray.length; i++) {
-      this.emails += this.imgarray[i] + ',';
-    }
-
-    let catid: number;
-    let prid: number;
-    let itname = this.validatee.validItems(itemnameinput);
-    let itmbrnd = this.validatee.validItems(itembrandinput);
-
-    this.addproductservice.getAddCategoryData()
-      .subscribe((result) => {
-          let catarray: Array<Categories> = result;
-          for (var i = 0; i < catarray.length; i++) {
-            if (catarray[i].category == itemcattype) {
-              catid = catarray[i].id;
-            }
-          }
-
-          // get product id relevent the product type
-          this.addproductservice.getProductData()
-            .subscribe((result) => {
-                let prarray: Array<Product> = result;
-                for (var i = 0; i < prarray.length; i++) {
-                  if (prarray[i].productType == itemprtype) {
-                    prid = prarray[i].id;
-                  }
-                }
-                let itmbj = new ItemsReport(itname, itmbrnd, itemqtyinput, itempriceinput, this.emails,
-                  'active', this.highlightModel, this.editorContentforSpecs, this.model, 0, this.imcount,
-                  itemrateinput, itemdisinput, this.selectedProId);
-                // function of item save
-                this.addproductservice.saveItemDetail(itmbj)
-                  .subscribe((result) => {
-                      this.emails = '';
-                      this.imgarray = [];
-                      this.imcount = '';
-                      Swal.fire(
-                        'Success',
-                        'Item Added Successful !',
-                        'success'
-                      );
-                    }, (error1 => {
-                      console.log(error1);
-                      Swal.fire(
-                        'Error',
-                        error1.error.error.message + '',
-                        'error'
-                      );
-                      console.log();
-                    })
-                  );
-
-              }, (error1 => {
-                console.log(error1);
-              })
-            );
-
-        }, (error1 => {
-          console.log(error1);
-        })
-      );
-  }
-
-  // ===================================================================================================================
-
-  // check product detail
-  saveProduct(prname, cattype) {
-    let catid = 0;
-    let pname = this.validatee.validProduct(prname);
-    Swal.fire({
-      title: 'Are you sure?',
-      text: 'did you fill correct specifications??',
-      type: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, I`m sure !'
-    }).then((result) => {
-      let slist = this.setSpecificationList();
+      let catid: number;
+      let prid: number;
+      let itname = this.validatee.validItems(itemnameinput);
+      let itmbrnd = this.validatee.validItems(itembrandinput);
 
       this.addproductservice.getAddCategoryData()
         .subscribe((result) => {
             let catarray: Array<Categories> = result;
             for (var i = 0; i < catarray.length; i++) {
-              if (catarray[i].category == cattype) {
+              if (catarray[i].category == itemcattype) {
                 catid = catarray[i].id;
               }
             }
-            let probj = new Product(pname, slist, catid);
 
-            // function of save product
-            this.addproductservice.saveProductDetail(probj)
+            // get product id relevent the product type
+            this.addproductservice.getProductData()
               .subscribe((result) => {
-                  this.mainspeclist = [];
-                  Swal.fire(
-                    'Success',
-                    'Product Added Successful !',
-                    'success'
-                  );
-                  this.getProductData();
+                  let prarray: Array<Product> = result;
+                  for (var i = 0; i < prarray.length; i++) {
+                    if (prarray[i].productType == itemprtype) {
+                      prid = prarray[i].id;
+                    }
+                  }
+                  let itmbj = new ItemsReport(itname, itmbrnd, itemqtyinput, itempriceinput, this.emails,
+                    'active', this.highlightModel, this.editorContentforSpecs, this.overViewModel, 0, this.imcount,
+                    itemrateinput, itemdisinput, this.selectedProId);
+                  // console.log('itmobj');
+                  // console.log(itmbj);
+                  // function of item save
+                  this.addproductservice.saveItemDetail(itmbj)
+                    .subscribe((result) => {
+                        this.emails = '';
+                        this.imgarray = [];
+                        this.imcount = '';
+                        Swal.fire(
+                          'Success',
+                          'Item Added Successful !',
+                          'success'
+                        );
+                      this.clearItem();
+                      }, (error1 => {
+                        console.log(error1);
+                        Swal.fire(
+                          'Error',
+                          error1.error.error.message + '',
+                          'error'
+                        );
+                        console.log();
+                      })
+                    );
                 }, (error1 => {
                   console.log(error1);
                 })
@@ -531,8 +650,61 @@ export class AddproductTabpanelContentComponent implements OnInit {
             console.log(error1);
           })
         );
-    });
+    }
   }
+
+  // ===================================================================================================================
+
+  // check product detail
+  saveProduct(prname, cattype) {
+    if (this.savingValidation(prname, cattype)) {
+      let catid = 0;
+      let pname = this.validatee.validProduct(prname);
+      Swal.fire({
+        title: 'Are you sure?',
+        text: 'did you fill correct specifications??',
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, I`m sure !'
+      }).then((result) => {
+        let slist = this.setSpecificationList();
+        this.addproductservice.getAddCategoryData()
+          .subscribe((result) => {
+              let catarray: Array<Categories> = result;
+              for (var i = 0; i < catarray.length; i++) {
+                if (catarray[i].category == cattype) {
+                  catid = catarray[i].id;
+                }
+              }
+              let probj = new Product(pname, slist, catid);
+
+              // function of save product
+              this.addproductservice.saveProductDetail(probj)
+                .subscribe((result) => {
+                    this.mainspeclist = [];
+                    Swal.fire(
+                      'Success',
+                      'Product Added Successful !',
+                      'success'
+                    );
+                    this.getProductData();
+                    this.clear();
+                  }, (error1 => {
+                    console.log(error1);
+                  })
+                );
+
+            }, (error1 => {
+              console.log(error1);
+            })
+          );
+      });
+    }
+  }
+
+
 
   //====================================================================================================================
 
@@ -540,6 +712,7 @@ export class AddproductTabpanelContentComponent implements OnInit {
   setSpecificationList(): any {
     let strlist = '';
     for (let u = 0; u < this.mainspeclist.length; u++) {
+      console.log(this.mainspeclist[u]);
       switch (this.mainspeclist[u]) {
         case 'imaging' :
           strlist += spect.imaging + '$';
@@ -573,6 +746,8 @@ export class AddproductTabpanelContentComponent implements OnInit {
           break;
       }
     }
+    // console.log('strlist comes');
+    // console.log(strlist);
     return strlist;
   }
 }
